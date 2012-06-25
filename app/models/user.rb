6f1e6fcd69eb5caeb4f3b8eeb4834c4b819925d0
Alex Kelly <alex.kelly@william-lee.com>
2012-06-25 15:07:03 +0100

    Add User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,                 presence: true, format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+                              
+  validates :password,              length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
 # == Schema Information
 #
